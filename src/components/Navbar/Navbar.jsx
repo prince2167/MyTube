2821afdeecc7500a9934b42834cfe425344681da
Prince <singhprince8222692@gmail.com>
2023-03-23 22:14:23 +0530

    Add functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,19 +2,23 @@ import React from "react";
 import classes from "./Navbar.module.css";
 import { HiOutlineMenu, AiFillYoutube } from "../../assets/icon";
 import { Link } from "react-router-dom";
+import { useVideo } from "../../contexts/index";
 const Navbar = () => {
+  const { dispatch } = useVideo();
+  const menuHandler = () => {
+    dispatch({ type: "MENU_OPEN" });
+  };
   return (
     <nav className={classes.navbar}>
       <div className={classes.logoContainer}>
-        <button className={classes.hamburger}>
-          <HiOutlineMenu  size="25" />
+        <button className={classes.hamburger} onClick={menuHandler}>
+          <HiOutlineMenu size="25" />
         </button>
-       
-          <Link className={classes.logo}>
-            <AiFillYoutube size="30" color="#ff3b30" />
-            <h3>MyTube</h3>
-          </Link>
-       
+
+        <Link className={classes.logo}>
+          <AiFillYoutube size="30" color="#ff3b30" />
+          <h3>MyTube</h3>
+        </Link>
       </div>
       <Link className={classes.account}>Login</Link>
     </nav>
