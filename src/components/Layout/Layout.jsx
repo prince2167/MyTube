702e839ffaa3ffdd3856455309f390fc7772be42
Prince <singhprince8222692@gmail.com>
2023-03-23 22:08:41 +0530

    Creating Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { useVideo } from "../../contexts";
 import { Navbar, Sidebar } from "../index";
-import classes from "./Layout.module.css"
+import classes from "./Layout.module.css";
 
 const Layout = () => {
+  const { isMenuOpen } = useVideo();
+
   return (
     <>
       <Navbar />
       <div className={classes.layout}>
-        <Sidebar />
-        <main style={{margin: "1rem 0 0 16rem" }}>
+        {isMenuOpen && <Sidebar />}
+        <main style={{ margin: "1rem 0 0 16rem" }}>
           <Outlet />
         </main>
       </div>
