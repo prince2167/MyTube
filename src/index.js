e9fc3ea9c5d63f91fa04df2561eaffd4087da078
Prince <singhprince8222692@gmail.com>
2023-03-23 22:26:58 +0530

    index

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-
+import { VideoProvider } from "./contexts";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <VideoProvider >
+        <App />
+      </VideoProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
