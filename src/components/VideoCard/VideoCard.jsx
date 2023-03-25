d63ec931eea8fce175b9666ae6166533128c2374
Prince <singhprince8222692@gmail.com>
2023-03-25 22:54:33 +0530

    create video card

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import classes from "./VideoCard.module.css";
+import { useVideo } from "../../contexts/index";
 const VideoCard = ({ video }) => {
+  const { isMenuOpen } = useVideo();
   const { snippet, statistics } = video;
   const { thumbnails, description, tags, title, channelTitle } = snippet;
-
+  
   return (
-    <div className={classes.cardContainer}>
+    <div
+      className={isMenuOpen ? classes.cardContainer2 : classes.cardContainer}
+    >
       <img src={thumbnails.medium.url} className={classes.thumbnail} />
-      <h4>{title}</h4>
-      <p>{channelTitle}</p>
-      <span>{statistics.viewCount} Views</span>
+      <div className={classes.cardDescription}>
+        <h4 className={classes.title}>{title}</h4>
+        <p>{channelTitle}</p>
+        <span>{statistics.viewCount} Views</span>
+      </div>
     </div>
   );
 };
