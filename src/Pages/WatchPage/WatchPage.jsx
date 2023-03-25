c0d8aa7199f128fd5641cc23356deda85e8edc6e
Prince <singhprince8222692@gmail.com>
2023-03-25 22:52:01 +0530

    embeded youtube video

--- src/Pages/WatchPage/WatchPage.jsx
+++ src/Pages/WatchPage/WatchPage.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useVideo } from "../../contexts";
 
 const WatchPage = () => {
-  return (
-    <div>WatchPage</div>
-  )
-}
+  const { dispatch, videos } = useVideo();
+  const { id } = useParams();
 
-export default WatchPage
+  useEffect(() => {
+    dispatch({ type: "MENU_CLOSED" });
+  }, []);
+  return (
+    <div>
+      <iframe
+        width="1000"
+        height="500"
+        src={`https://www.youtube.com/embed/${id}`}
+        title="YouTube video player"
+        play={true}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default WatchPage;
