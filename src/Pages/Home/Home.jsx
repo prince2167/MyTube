9e8d1d3dfcfb2b9596f042ce90d77c45f00bc0ee
Prince <singhprince8222692@gmail.com>
2023-03-25 22:58:50 +0530

    Add conditional class

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { VideoCard } from "../../components/index";
 import { useVideo } from "../../contexts";
-import classes from "./Home.module.css"
+import classes from "./Home.module.css";
 const Home = () => {
-  const { videos } = useVideo();
-
+  const { dispatch, videos } = useVideo();
+  useEffect(() => {
+    dispatch({ type: "MENU_ALWAYS_OPEN" });
+  }, []);
   return (
-    <div className={classes.homeContainer} >
+    <div className={classes.homeContainer}>
       {videos.map((video) => (
-        <Link to={`watch-page/${video.id}`}>
+        <Link to={`watch-page/${video.id}`} className={classes.cardLink}>
           <VideoCard key={video.id} video={video} />
         </Link>
       ))}
